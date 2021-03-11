ee2174a0a97c8958bf261b34f678839f6b244672
Scott C <78004109+curry-scott@users.noreply.github.com>
2021-03-11 15:23:43 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const board = document.getElementById("board");
+
+
+class view {
+    constructor() {
+        function init_view() {
+
+            board.innerHTML = '';
+          
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+class model {
+    constructor() {
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+class controller {
+    constructor({
+
+
+    })
+
+}
